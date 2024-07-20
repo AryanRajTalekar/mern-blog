416ba83acba42fbf108f3612cfd2b50553657015
AryanRajTalekar <156109321+AryanRajTalekar@users.noreply.github.com>
2024-07-20 18:07:41 +0530

    created routes and pages basic frames

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
 
-function App() {
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Dashboard from './routes/Dashboard';
+import Projects from './routes/Projects';
+import SignIn from './routes/SignIn';
+import SignUp from './routes/SignUp';
+
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
